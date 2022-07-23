1dfadc5ce062a60d8bba9ec8cf41357a611688e4
Jedzu <annenusia@gmail.com>
2022-07-23 13:25:27 +0200

    Klasa ForLoop z pętlą z tablicy kotków

---
+++ src/ForLoop.java
@@ -0,0 +1,13 @@
+public class ForLoop {
+
+    public static void main(String[] args) {
+
+        String[] kittyBoard = new String[]{"futro","korba","bubu","tofik","tomtom"};
+
+ for (int i = 0; i< kittyBoard.length; i++){
+     System.out.println(kittyBoard[i]);
+
+ }
+
+    }
+}
